3034cae706cd35fff6df244bd2c814bab5cb782d
Biotualetnij <66161409+1w3q2e4t6r5y@users.noreply.github.com>
2020-11-26 13:11:32 +0200

    "L-1 commit"

---
+++ animations/animation.js
@@ -0,0 +1,17 @@
+var  Login = function(){
+	
+	document.getElementById("whiteElement").id="whiteElementL";
+
+	document.getElementById("reg").id="reghidden";
+	
+	document.getElementById("loghidden").id="log";
+
+}
+
+var  SingUp = function(){
+
+	document.getElementById("whiteElementL").id="whiteElement";
+	document.getElementById("log").id="loghidden";
+	document.getElementById("reghidden").id="reg";
+	
+}
